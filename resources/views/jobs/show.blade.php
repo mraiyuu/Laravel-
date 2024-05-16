4c1ef1cf73757d5a56ee6c1233759d7e6c4cc2da
mraiyuu <munenedenis618@gmail.com>
2024-05-16 15:07:50 +0300

    update and delete resource

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -2,8 +2,12 @@
     <x-slot:heading>
         Job
     </x-slot:heading>
-    
-    <h2 class="font-bold text-lg">{{ $job['title'] }}</h2>
-    <P>This job pays {{ $job['salary']}} per year.</P>
+
+    <h2 class="font-bold text-lg">{{ $job->title }}</h2>
+    <P>This job pays {{ $job->salary}} per year.</P>
+
+    <p class="mt-10">
+        <x-button href="/jobs/{{ $job->id}}/edit">Edit job</x-button>
+    </p>
 
 </x-layout>
