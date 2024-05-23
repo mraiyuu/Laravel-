485758736a148c01613d7da320edf702f5eebef1
mraiyuu <munenedenis618@gmail.com>
2024-05-23 23:09:11 +0300

    authorization refactor

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Job;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
 
 class JobController extends Controller
 {
@@ -42,6 +45,16 @@ class JobController extends Controller
 
     public function edit(Job $job)
     {
+        Gate::define('edit-job', function (User $user, Job $job) {
+           return $job->employer->user->is($user);
+                
+        });
+
+        if(Auth::guest()) {
+            return redirect('/login');
+        }
+        Gate::authorize('edit-job', $job);
+        
         return view('jobs.edit', ['job' => $job]);
     }
 
