1d342ec78e197247576a3aa48416560bebde66a7
mraiyuu <munenedenis618@gmail.com>
2024-05-28 16:02:48 +0300

    refactor

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ Route::get('/test', function() {
 
 
 Route::view('/', 'home');
-
+//views
 Route::get('/jobs', [JobController::class, 'index']);
 Route::get('/jobs/create', [JobController::class, 'create']);
 Route::get('/jobs/{job}', [JobController::class, 'show'])->middleware('auth');
